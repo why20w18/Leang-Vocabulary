46f3d79bac3714fe7f16a331f39db30f28701503
why20w18 <enco3162@gmail.com>
2024-10-16 18:50:48 +0300

    home_frame slotlari define edilmeye baslandi

--- src/wx_gui.cpp
+++ src/wx_gui.cpp
@@ -9,13 +9,13 @@ int login_frame::isLoginButtonDatabaseConnection = 0;
 
 enum ID_ENUM_LOGIN{
    ID_LoginSumbit = 1,
-   ID_RegisterSumbit
+   ID_RegisterSumbit,
 };
 
 //EVENT TABLE MAKROLARI - bunun yerine dinamik bind kullanilabilirdi
 wxBEGIN_EVENT_TABLE(login_frame,wxFrame)
    EVT_BUTTON(ID_LoginSumbit,login_frame::OnLoginSumbitButton) //func adres
-   EVT_BUTTON(ID_RegisterSumbit,login_frame::OnRegisterSumbitButton) 
+   EVT_BUTTON(ID_RegisterSumbit,login_frame::OnRegisterSumbitButton)
    
 wxEND_EVENT_TABLE()
 
@@ -26,20 +26,20 @@ login_frame::login_frame(const wxString &tittle ,leangEngine &leTransfer)
    this->mevcutPencereTittle = tittle;
    this->leangVersion = "0.1";
 
-   //CONFIG KONSOL INFO
+//CONFIG KONSOL INFO
    std::cout << "MIN_SIZE=" << leTransfer.minSizeArr[0] << "," << leTransfer.minSizeArr[1] << std::endl; 
    SetMinSize(wxSize(leTransfer.minSizeArr[0],leTransfer.minSizeArr[1]));     
 
    std::string cfgDurum = (leTransfer.loginGuiActive == 1) ? "TRUE" : "FALSE";  
    std::cout << "GUI_ACTIVE=" << cfgDurum << std::endl; 
 
-   //GUI ISLEMLERI
+
+//GUI ISLEMLERI
    wxPanel *panel = new wxPanel(this,wxID_ANY);
    //LOGIN KISMI
    //-->label(wxstatictext)
    //-->textctrl
-   
-   //KULLANICI_ADI
+
    wxStaticText *label_username = new wxStaticText(panel,wxID_ANY,"KULLANICI ADI:",wxPoint(18,85));
    userName = new wxTextCtrl(panel,wxID_ANY,"",wxPoint(135,75),wxSize(200,40));
    //genislik = 134...311 = 177
@@ -48,15 +48,26 @@ login_frame::login_frame(const wxString &tittle ,leangEngine &leTransfer)
    wxStaticText *label_pass = new wxStaticText(panel,wxID_ANY,"PAROLA :",wxPoint(18,183));
    userPassword = new wxTextCtrl(panel,wxID_ANY,"",wxPoint(135,175),wxSize(200,40),wxTE_PASSWORD);
 
+
+//BUTONLAR
    buttonGiris = new wxButton(panel,ID_LoginSumbit,"Login",wxPoint(120,281),wxSize(134,36));
    //108,281 = x,y
    //h:317..281 = 36 
    //w:108..242 = 134
 
    buttonRegister = new wxButton(panel,ID_RegisterSumbit,"Register",wxPoint(146,320),wxSize(80,25));
-
-   wxStaticText *leangVersionLabel = new wxStaticText(panel,wxID_ANY,"Leang Version : "+this->leangVersion,wxPoint(130,370));
+   
+   
+   wxStaticText *leangVersionLabel = new wxStaticText(panel,wxID_ANY,"Leang Version : "+this->leangVersion,wxPoint(130,370));   
 }
+
+/////////////////////////////////////////////////////////LOGIN_FRAME_SLOT_FONKSIYONLARI////////////////////////////////////
+/////////////////////////////////////////////////////////LOGIN_FRAME_SLOT_FONKSIYONLARI////////////////////////////////////
+/////////////////////////////////////////////////////////LOGIN_FRAME_SLOT_FONKSIYONLARI////////////////////////////////////
+/////////////////////////////////////////////////////////LOGIN_FRAME_SLOT_FONKSIYONLARI////////////////////////////////////
+/////////////////////////////////////////////////////////LOGIN_FRAME_SLOT_FONKSIYONLARI////////////////////////////////////
+/////////////////////////////////////////////////////////LOGIN_FRAME_SLOT_FONKSIYONLARI////////////////////////////////////
+/////////////////////////////////////////////////////////LOGIN_FRAME_SLOT_FONKSIYONLARI////////////////////////////////////
 
 void login_frame::OnLoginSumbitButton(wxCommandEvent &e){
    //buton verileri class icine cekmemizi saglayacak attributeslari alacagiz
@@ -167,6 +178,9 @@ void login_frame::errMessage(int hataTip,const std::string &m){
 /////////////////////////////////////////////HOME_FRAME_CLASS_DEFINE///////////////////////////////////////////////////
 /////////////////////////////////////////////HOME_FRAME_CLASS_DEFINE///////////////////////////////////////////////////
 /////////////////////////////////////////////HOME_FRAME_CLASS_DEFINE///////////////////////////////////////////////////
+/////////////////////////////////////////////HOME_FRAME_CLASS_DEFINE///////////////////////////////////////////////////
+/////////////////////////////////////////////HOME_FRAME_CLASS_DEFINE///////////////////////////////////////////////////
+/////////////////////////////////////////////HOME_FRAME_CLASS_DEFINE///////////////////////////////////////////////////
 
 enum ID_ENUM_HOME{
     ID_SETTINGS_OZELLESTIRME = 11,
@@ -185,7 +199,7 @@ enum ID_ENUM_HOME{
 };
 
 wxBEGIN_EVENT_TABLE(home_frame,wxFrame)
-   //EVT_MENU()
+   EVT_MENU(ID_SUPPORT_GITHUB,home_frame::slotSupportGithub)
 wxEND_EVENT_TABLE()
 
 home_frame::home_frame(const wxString &title) : wxFrame(nullptr,wxID_ANY,title,wxDefaultPosition)
@@ -256,3 +270,27 @@ home_frame::home_frame(const wxString &title) : wxFrame(nullptr,wxID_ANY,title,w
 }
 
 
+////////////////////////////////////////////////HOME_FRAME_SLOT_FONKSIYONLARI///////////////////////////////////////////////////
+////////////////////////////////////////////////HOME_FRAME_SLOT_FONKSIYONLARI///////////////////////////////////////////////////
+////////////////////////////////////////////////HOME_FRAME_SLOT_FONKSIYONLARI///////////////////////////////////////////////////
+////////////////////////////////////////////////HOME_FRAME_SLOT_FONKSIYONLARI///////////////////////////////////////////////////
+////////////////////////////////////////////////HOME_FRAME_SLOT_FONKSIYONLARI///////////////////////////////////////////////////
+
+
+void home_frame::slotSettingsOzellestirme(wxCommandEvent &e){}
+void home_frame::slotSettingsBildirim(wxCommandEvent &e){}
+void home_frame::slotSettingsProgramDili(wxCommandEvent &e){}
+
+void home_frame::slotSupportGithub(wxCommandEvent &e){
+   //mesaj kutusunu ortalayarak cikarttik
+   wxMessageDialog(this,"https://github.com/why20w18/Leang-Vocabulary\n\
+   ADRESINDEN KATKIDA BULUNABILIR PROGRAMDAKI BUGLARI BILDIREBILIRSIN","Support-Github",wxOK | wxCENTRE).ShowModal();
+}
+    
+void home_frame::slotLeangKelimeTabani(wxCommandEvent &e){}
+void home_frame::slotLeangIstekDil(wxCommandEvent &e){}
+void home_frame::slotLeangKullaniciNotlari(wxCommandEvent &e){}
+    
+void home_frame::slotLeangKelimeSetleri(wxCommandEvent &e){}
+void home_frame::slotLeangExport(wxCommandEvent &e){}
+void home_frame::slotLeangImport(wxCommandEvent &e){}
