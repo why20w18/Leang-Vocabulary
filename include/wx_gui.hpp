e1a6ff1fa5a74529feaac280a7f4253cab6f925b
why20w18 <enco3162@gmail.com>
2024-10-17 16:35:03 +0300

    settings_frame tercihler kismi olusturuldu slotlar baglanmadi , home_frame tureme kaldirildi

--- include/wx_gui.hpp
+++ include/wx_gui.hpp
@@ -73,7 +73,7 @@ private:
 
 class home_frame : public wxFrame , public leangEngine{
 public:
-    //constructor ve wxframeden turedigi icin o classinda constructorini baslatma 
+    //constructor ve wxframeden turedigi icin o classinda constructorini baslatma
     home_frame(const wxString &yeniPencere_Baslik);
 
 private:
@@ -85,26 +85,74 @@ private:
     wxMenu *menuSupport;  //support : github contributes
     wxMenu *menuLeang;    //leang   : kelime_tabani , ogrenilmek_istenen_diller , kelime_setleri , export , import , user_notes
     
+    wxButton *leang_baslat; //leang.cpp'ye bagli fonksiyonlari calistirmali
+
+
     //SLOTLAR-MENU SLOTLARI
-    void slotSettingsOzellestirme(wxCommandEvent &e);
+    void slotSettingsTercihler(wxCommandEvent &e);
     void slotSettingsBildirim(wxCommandEvent &e);
     void slotSettingsProgramDili(wxCommandEvent &e);
 
+    
     void slotSupportGithub(wxCommandEvent &e);
+    
     
     void slotLeangKelimeTabani(wxCommandEvent &e);
     void slotLeangIstekDil(wxCommandEvent &e);
     void slotLeangKullaniciNotlari(wxCommandEvent &e);
-    
     void slotLeangKelimeSetleri(wxCommandEvent &e);
     void slotLeangExport(wxCommandEvent &e);
     void slotLeangImport(wxCommandEvent &e);
 
+
+protected:
     //SLOTLAR-KOMPONENT SLOTLARI (LEANG MOTORUNDAN GELECEK)
 
 
     //home_frame'ye ozel event_table deklerasyonu
     wxDECLARE_EVENT_TABLE();
 };
+
+//MENU PENCERELERI
+///////////////////////////////////////////////settings_CLASS-SETTINGS//////////////////////////////////////////////////
+///////////////////////////////////////////////settings_CLASS-SETTINGS//////////////////////////////////////////////////
+///////////////////////////////////////////////settings_CLASS-SETTINGS//////////////////////////////////////////////////
+
+class settings_frame : public wxFrame{
+public:
+    static bool pencereAcikMi;
+    
+    //alt pencere 1'den baslar bir menuBar icinde kac adet menu varsa oraya kadar gider
+    settings_frame(const wxString &tittle , int altPencereNo);
+    int getAltPencereNo();
+
+private:
+
+////TERCIHLER = PREFERENCES////
+    bool isUserWantUpdate;
+    wxString minSizeLogin;
+    wxString minSizeHome;
+    
+    wxCheckBox *checkBox_autoUpdate;
+    wxCheckBox *checkBox_loginGuiActive;
+    wxCheckBox *checkBox_homeGuiActive;
+    
+    
+    wxTextCtrl *textCTRL_loginSize;
+    wxTextCtrl *textCTRL_homeSize;
+
+    wxButton *button_kaydetTercih;
+
+    //PREFERENCES-SLOTLAR//
+    
+
+    //PREFERENCES-SLOTLAR//
+
+////TERCIHLER = PREFERENCES////
+
+    void  OnSettingsClose(wxCloseEvent &e);
+
+};
+
 
 #endif 
